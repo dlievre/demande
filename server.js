4793a6772176770695541bce64c734ddbc197685
dlievre <42@photeam.com>
2017-03-10 17:53:29 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -18,57 +18,105 @@ app.use(bodyParser.urlencoded({extended: true})) // parsing application x/www-fo
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
+// inclure la gestion des sessions
+let session = require('express-session')
+//app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'mykey',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+// pour une utilisation de session en production : utiliser redis : https://codeforgeek.com/2015/07/using-redis-to-handle-session-in-node-js/
+
+
+
 // ***** moteur template *****
 app.set('view engine', 'ejs')
 app.use(express.static('public')) // route static du dossier public
 
 var init = 'init';
 
+// ***** routes *****
 app.post('/', upload.array(), (request, response) => {
+	var sess = request.session
 	// if (request.body.message === undefined || request.body.message === ''){
 	// 	response.render('pages/index', {error: "vous n'avez pas saisi de message :"})
-	// }
+	// } 
+	var info = new Object()
+	info.titre_demande = "Dernières demandes"
 
-		if (request.body.accident === 'ok' ){
+		if (request.body.voirie){
 			init = 'non'
-		response.render('pages/index', {niv: "accident", msg:"accident" })
+			response.render('pages/index', {niv: "voirie", msg:"Intervention voirie", info })
+	}
+			if (request.body.voirie_encombrant){
+			response.render('pages/index', {niv: "voirie_encombrant", msg:"Intervention voirie - Encombrant", saisie: 'ok', info })
+	}
+
+		if (request.body.accident){
+			response.render('pages/index', {niv: "accident", msg:"Information : accident", saisie: 'no', info })
 	}
 		if (request.body.accident_route === 'ok' ){
-		response.render('pages/index', {niv: "accident_route", msg:"accident de la route" })
+			response.render('pages/index', {niv: "accident_route", msg:"Information : accident de la route", saisie: 'no', info })
 	}
 		if (request.body.accident_grave === 'ok' ){
-		response.render('pages/index', {niv: "accident_grave", msg:"accident grave de la route" })
+			response.render('pages/index', {niv: "accident_grave", msg:"Information :  accident grave de la route", saisie: 'ok' })
 	}
 
+			if (request.body.litige === 'ok' ){
+			init = 'non'
+			response.render('pages/index', {niv: "litige", msg:"Information : litige", btn: ['interne', 'client', 'commercial'] })
+	}
+
+		if (request.body.message){
+			var msg = 'Votre message est transmis : '+request.body.message
+		response.render('pages/index', {msg, saisie: 'no', info  })
+	}
 		if (request.body.aide === 'ok' ){
 			var msg = 'débrouille-toi'
 		response.render('pages/index', {msg})
 	}
 
+
+
 	// 		if (request.body.urgence == true){
 	// 	console.log('urgence niveau2')
 	// 	response.render('pages/index', {niv: "niv2" })
 	// }
+	console.log('Post')
 	console.log('init = '+init)
 	console.log(request.body)
 	//console.log(request.body.aide)
 	//console.log('aide = '+request.body.aide)
+	console.log('session = '+sess)
+	console.log('Cookies: ', request.cookies)
+	console.log('Signed Cookies: ', request.signedCookies)
 	console.log('message = '+request.body.message)
-	console.log('urgence = '+request.body.accident)
+	console.log('accident = '+request.body.accident)
+	console.log('voirie = '+request.body.voirie)
 	console.log('niv = '+request.body.niv)
 
 	//console.log('niv '+request.body.niv)
 })
 
-// ***** routes *****
+app.get('/map', upload.array(), (request, response) => {
+	var sess = request.session
+	response.render('pages/map', {msg: '3, rue Baudin - Levallois Perret'})
+})
+
+
 app.get('/', (request, response) => {
-	console.log('Cookies: ', request.cookies)
-	console.log('Signed Cookies: ', request.signedCookies)
+
 	var test = Date.now();
 	var test = Date('year')
 	//response.render('pages/index', {test: 'salut'})
-	response.render('pages/index', {test})
-	//response.send('ok')
+	var info = new Object()
+	info.titre_demande = "Dernières demandes"
+	info.titre = 'info'
+	info.text = "Choisissez l'évènement"
+	response.render('pages/index', {info})
+	console.log('Get = '+request.body)
 })
 
 app.listen(8081)
